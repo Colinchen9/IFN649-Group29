a04fd075a9273b28b6b4e3be23199821261b877a
Colinchen9 <82505505+Colinchen9@users.noreply.github.com>
2021-08-19 23:19:51 +1000

    Create Hello world.ino

---
+++ Week 2&3/Hello world.ino
@@ -0,0 +1,12 @@
+// C++ code
+//
+void setup()
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Hello world");
+    delay(5000); // Delay a little bit to improve simulation performance
+}
